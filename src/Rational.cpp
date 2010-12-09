a52dbaaab7f8b6c36ac7455feab41745e65c3e1a
Chris Dunphy <chris.dunphy@sjrb.ca>
2010-12-09 16:46:09 -0700

    We don't want to aggressively inline functions that will be called a lot.

---
+++ src/Rational.cpp
@@ -0,0 +1,55 @@
+#include "Rational.h"
+
+namespace rcd {
+
+const std::string Rational::to_str() const {
+	std::stringstream sout;
+	sout << numer;
+	/* Only display the fraction part if it is required... */
+	if (denom > 1) {
+		sout << "/" << denom;
+	}
+	return sout.str();
+}
+
+void Rational::reduce() {
+	int g = gcd(numer, denom);
+	numer /= g;
+	denom /= g;
+
+	if (numer < 0 && denom < 0) {
+		abs(numer);
+		abs(denom);
+	}
+}
+
+inline const Rational reciprocal(const Rational& r) {
+	return Rational(r.denominator(), r.numerator());
+}
+
+std::ostream& operator<<(std::ostream& out, const Rational& r) {
+	return out << r.to_str();
+}
+
+const Rational operator+(const Rational& lhs, const Rational& rhs) {
+	int l = lcm(lhs.denominator(), rhs.denominator());
+	return Rational((lhs.numerator() * l / lhs.denominator())
+			+ (rhs.numerator() * l / rhs.denominator()), l);
+}
+
+const Rational operator-(const Rational& lhs, const Rational& rhs) {
+	int l = lcm(lhs.denominator(), rhs.denominator());
+	return Rational((lhs.numerator() * l / lhs.denominator())
+			- (rhs.numerator() * l / rhs.denominator()), l);
+}
+
+const Rational operator*(const Rational& lhs, const Rational& rhs) {
+	return Rational(lhs.numerator() * rhs.numerator(), lhs.denominator()
+			* rhs.denominator());
+}
+
+const Rational operator/(const Rational& lhs, const Rational& rhs) {
+	return lhs * reciprocal(rhs);
+}
+
+}
--- src/Rational.h
+++ src/Rational.h
@@ -68,70 +68,34 @@ inline int lcm(int x, int y) {
 /*
  * This function returns a nice string representation of a rational number.
  */
-inline const std::string Rational::to_str() const {
-		std::stringstream sout;
-		sout << numer;
-		/* Only display the fraction part if it is required... */
-		if (denom > 1) {
-			sout << "/" << denom;
-		}
-		return sout.str();
-	}
+const std::string Rational::to_str();
 
 /*
  * This function will reduce our fraction, even eliminating the denominator
  * if it is not needed.  If both the numerator and denominator are negative, it
  * will flip the sign for us as well.
  */
-inline void Rational::reduce() {
-		int g = gcd(numer, denom);
-		numer /= g;
-		denom /= g;
-
-		if (numer < 0 && denom < 0) {
-			abs(numer);
-			abs(denom);
-		}
-	}
+inline void Rational::reduce();
 
 /*
  * The reciprocal of a rational number A/B = B/A.
  */
-inline const Rational reciprocal(const Rational& r) {
-	return Rational(r.denominator(), r.numerator());
-}
+const Rational reciprocal(const Rational& r);
+
 
 /*
  * This lets us feed Rationals into ostreams for IO
  */
-inline std::ostream& operator<<(std::ostream& out, const Rational& r) {
-	return out << r.to_str();
-}
+std::ostream& operator<<(std::ostream& out, const Rational& r);
 
 /*
  * The following functions implement arithmetic operators...
  */
+const Rational operator+(const Rational& lhs, const Rational& rhs);
+const Rational operator-(const Rational& lhs, const Rational& rhs);
+const Rational operator*(const Rational& lhs, const Rational& rhs);
+const Rational operator/(const Rational& lhs, const Rational& rhs);
 
-inline const Rational operator+(const Rational& lhs, const Rational& rhs) {
-	int l = lcm(lhs.denominator(), rhs.denominator());
-	return Rational((lhs.numerator() * l / lhs.denominator()) + (rhs.numerator()
-			* l / rhs.denominator()), l);
-}
-
-inline const Rational operator-(const Rational& lhs, const Rational& rhs) {
-	int l = lcm(lhs.denominator(), rhs.denominator());
-	return Rational((lhs.numerator() * l / lhs.denominator()) - (rhs.numerator()
-			* l / rhs.denominator()), l);
-}
-
-inline const Rational operator*(const Rational& lhs, const Rational& rhs) {
-	return Rational(lhs.numerator() * rhs.numerator(), lhs.denominator()
-			* rhs.denominator());
-}
-
-inline const Rational operator/(const Rational& lhs, const Rational& rhs) {
-	return lhs * reciprocal(rhs);
-}
 
 }
 
